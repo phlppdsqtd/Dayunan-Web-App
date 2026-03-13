1bd4fe60ec67ffeda2805a02e7abbee1e80cce55
phlppdsqtd <philippdesquitado@gmail.com>
2026-03-14 01:42:44 +0800

    Week 1: Minor changes

--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -32,14 +32,14 @@ class PackageSeeder extends Seeder
             'title' => 'Large Event (22 Hours)',
             'description' => "• More than 24 guests allowed\n• Beds for up to 12 guests\n• Tables and chairs for 24 pax included\n• Access for set-up of catering service and extra tables/chairs allowed\n• 22-Hour Stay: ₱25,000 (2:00 PM to 12:00 NN)",
             'price' => 25000.00,
-            'max_guests' => 100,
+            'max_guests' => 80,
         ]);
 
         Package::create([
             'title' => 'Large Event (12 Hours)',
             'description' => "• More than 24 guests allowed\n• Beds for up to 12 guests\n• Tables and chairs for 24 pax included\n• Access for set-up of catering service and extra tables/chairs allowed\n• 12-Hour Stay: ₱21,000 (2:00 PM to 2:00 AM)",
             'price' => 21000.00,
-            'max_guests' => 100,
+            'max_guests' => 80,
         ]);
 
         // --- LARGE EVENT WITH SOUND AND LIGHTS ---
@@ -47,14 +47,14 @@ class PackageSeeder extends Seeder
             'title' => 'Large Event with Sound & Lights (22 Hours)',
             'description' => "• More than 24 guests allowed\n• Beds for up to 12 guests\n• Tables and chairs for 24 pax included\n• Professional sound and lights system with operator included\n• Access for catering set-up and extra tables/chairs allowed\n• 22-Hour Stay: ₱37,000 (2:00 PM to 12:00 NN)",
             'price' => 37000.00,
-            'max_guests' => 100,
+            'max_guests' => 80,
         ]);
 
         Package::create([
             'title' => 'Large Event with Sound & Lights (12 Hours)',
             'description' => "• More than 24 guests allowed\n• Beds for up to 12 guests\n• Tables and chairs for 24 pax included\n• Professional sound and lights system with operator included\n• Access for catering set-up and extra tables/chairs allowed\n• 12-Hour Stay: ₱33,000 (2:00 PM to 2:00 AM)",
             'price' => 33000.00,
-            'max_guests' => 100,
+            'max_guests' => 80,
         ]);
     }
 }
